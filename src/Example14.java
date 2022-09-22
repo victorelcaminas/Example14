8803522c0f5cef72d28589f791a77b4193b25b85
Victor Alonso <victor@ieselcaminas.org>
2022-09-22 09:50:56 +0200

    Indentation

--- src/Example14.java
+++ src/Example14.java
@@ -14,20 +14,18 @@ public class Example14 {
         System.out.println("Enter the third number");
         z = inputValue.nextInt();
 
-            if (x <= y && y <= z) {
-                System.out.println(x + "," + y + "," + z);
-            } else if (x <= z && z <= y) {
-                System.out.println(x + "," + z + "," + y);
-            } else if (y <= x && x <= z) {
-                System.out.println(y + "," + x + "," + z );
-            } else if (y <= z && z <= x) {
-                System.out.println(y + "," + z + "," + x);
-            } else if (z <= x && x <= y) {
-                System.out.println(z + "," + x + "," + y);
-            } else {
-                System.out.println(z + "," + y + "," + x);
-            }
-
-
+        if (x <= y && y <= z) {
+            System.out.println(x + "," + y + "," + z);
+        } else if (x <= z && z <= y) {
+            System.out.println(x + "," + z + "," + y);
+        } else if (y <= x && x <= z) {
+            System.out.println(y + "," + x + "," + z );
+        } else if (y <= z && z <= x) {
+            System.out.println(y + "," + z + "," + x);
+        } else if (z <= x && x <= y) {
+            System.out.println(z + "," + x + "," + y);
+        } else {
+            System.out.println(z + "," + y + "," + x);
         }
+
     }
